a7cb3ac3ce6fdc5101a1bb4abdde1cb5d8264581
UkuRenekKronbergs <uku.renek.kronbergs@ut.ee>
2025-10-26 18:28:41 +0200

    Forgot to save last time (Battle Scene and build))

--- Assets/Scripts/Ship/PlayerController.cs
+++ Assets/Scripts/Ship/PlayerController.cs
@@ -5,13 +5,6 @@ namespace Airships.Ship
     public class PlayerController : MonoBehaviour
     {
         public GameObject location;
-        Transform transform;
-
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-            transform = gameObject.GetComponent<Transform>();
-        }
 
         // Update is called once per frame
         void Update()
@@ -24,8 +17,8 @@ namespace Airships.Ship
 
             location = destination;
 
-            Vector3 destinationPos = destination.GetComponent<Transform>().position;
-            transform.position = destinationPos + new Vector3(0, 1, 0);
+            var destinationPos = destination.transform.position;
+            transform.position = destinationPos + new Vector3(0f, 1f, 0f);
         }
     }
 }
